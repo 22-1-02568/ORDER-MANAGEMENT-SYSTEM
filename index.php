127ca034dddf6b948c121b2b18915b67fd93e496
22-1-02568 <167276614+22-1-02568@users.noreply.github.com>
2024-05-03 22:06:12 +0800

    index.php

--- index.php
+++ index.php
@@ -55,17 +55,21 @@
         <div class="menu">
             <h2>Menu</h2>
             <ul>
-                <li>Pawis ni Heiroll - PHP 10,000</li>
-                <li>Luha ni Heiroll - PHP 50,000</li>
-                <li>Bathwater ni Heiroll - PHP 20,000</li>
+                <li>Halo-Halo - PHP 50</li>
+                <li>Mais Con Yelo - PHP 40</li>
+                <li>Saging Con Yelo - PHP 40</li>
+                <li>Sago't Gulaman - PHP 20</li>
+                <li>Buko Juice - PHP 20</li>
             </ul>
         </div>
         <form method="post" action="">
             <label for="item">Select Item:</label>
             <select id="item" name="item">
-                <option value="10000">Pawis ni Heiroll - PHP 10,000</option>
-                <option value="50000">Luha ni Heiroll - PHP 50,000</option>
-                <option value="20000">Bathwater ni Heiroll - PHP 20,000</option>
+                <option value="50">Halo-Halo - PHP 50</option>
+                <option value="40">Mais Con Yelo - PHP 40</option>
+                <option value="40">Saging Con Yelo - PHP 40</option>
+                <option value="20">Sago't Gulaman - PHP 20</option>
+                <option value="20">Buko Juice - PHP 20</option>
             </select><br>
             <label for="quantity">Quantity:</label>
             <input type="number" id="quantity" name="quantity" min="1" required><br>
